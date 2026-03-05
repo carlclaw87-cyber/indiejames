4b47f8cdd8a90b173f2dbf178f6ab972f7c90d0e
carlclaw87-cyber <carlclaw87@gmail.com>
2026-03-05 12:32:59 -0500

    Create layout.jsx

---
+++ package.json/app/layout.jsx
@@ -0,0 +1,13 @@
+import "./globals.css";
+
+export const metadata = {
+title: "IndieJames",
+description: "Indie milestone curator"
+};
+
+export default function RootLayout({ children }) {return (
+<html lang="en">
+<body>{children}</body>
+</html>
+);
+}
